42527f92a6848d85c67ffd9ddbf807925e699dc3
ala_ping <arasakinpro@gmail.com>
2020-06-04 22:04:47 +0900

    docusaurusのindex.jsにつながらない

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
 import * as functions from 'firebase-functions';
+const Home = require('./docusaurus/src/page');
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const portfolio = functions.https.onRequest((request, response) => {
+ response.send(Home);
 });
